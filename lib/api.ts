72efc006a075314267b77118ee04560f70e1e69f
Ojukwu-Chinedu <levdevoj17@gmail.com>
2026-07-14 11:09:18 +0100

    feat: add API error interceptor with toast notifications for failed requests

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { toast } from "sonner";
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8000/api",
@@ -18,6 +19,19 @@ api.interceptors.request.use((config) => {
   }
   return config;
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const message =
+      error.response?.data?.message ??
+      error.response?.data?.error ??
+      error.message ??
+      "An unexpected error occurred";
+    toast.error(message);
+    return Promise.reject(error);
+  }
+);
 
 export interface WalletSummary {
   totalBalance: string;
